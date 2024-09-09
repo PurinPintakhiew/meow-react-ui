baac9ac3b943bdb23296b35218a7db44bc483a92
PurinPintakhiew <dev.purin@gmail.com>
2024-09-09 13:28:23 +0700

    feat : InputTextArea

---
+++ src/components/InputTextArea.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { cn } from "../libs/cn";
+
+interface InputProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const InputTextArea = ({ className, ...props }: InputProps) => {
+  return <textarea className={cn("border !text-black rounded-sm", className)} {...props} />;
+};
+
+export default InputTextArea;
