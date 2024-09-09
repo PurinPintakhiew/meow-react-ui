cb6b1f0594409c9b13cb3250b49371962259ea6e
PurinPintakhiew <dev.purin@gmail.com>
2024-09-09 13:28:05 +0700

    feat : InputText

---
+++ src/components/InputText.tsx
@@ -0,0 +1,10 @@
+import { cn } from "../libs/cn";
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const InputText = ({ className, ...props }: InputProps) => {
+  return <input type="text" className={cn("border h-8 !text-black rounded-sm", className)} {...props} />;
+};
+
+export default InputText;
