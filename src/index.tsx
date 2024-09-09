61b2443ef10e437f41e568e5f0c85cd6b854433e
PurinPintakhiew <dev.purin@gmail.com>
2024-09-09 13:33:48 +0700

    feat : call components

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
-export { default as Button } from "./components/Button";
+import Button from "./components/Button";
+import InputText from "./components/InputText";
+import InputTextArea from "./components/InputTextArea";
+
+export { Button, InputText, InputTextArea };
