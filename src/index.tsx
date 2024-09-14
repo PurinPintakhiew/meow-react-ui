81eb6ee02263e8996be2399e4b92ac26f6f56152
PurinPintakhiew <dev.purin@gmail.com>
2024-09-14 23:44:33 +0700

    feat : add styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import Button from "./components/Button";
-import InputText from "./components/InputText";
-import InputTextArea from "./components/InputTextArea";
+import "@/styles/global.css";
+import Button from "@/components/Button";
+import InputText from "@/components/InputText";
+import InputTextArea from "@/components/InputTextArea";
 
 export { Button, InputText, InputTextArea };
