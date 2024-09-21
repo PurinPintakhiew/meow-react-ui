6bd3dd24cdbb4138daf6749e8d6f70921fd2d1c2
PurinPintakhiew <dev.purin@gmail.com>
2024-09-21 17:52:26 +0700

    build : rebuild

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    label?: string;
+    type?: "button" | "submit" | "reset";
+    className?: string;
+    children?: React.ReactNode;
+}
+declare const Button: ({ label, type, className, children, ...props }: ButtonProps) => React.JSX.Element;
+
+export { Button };
